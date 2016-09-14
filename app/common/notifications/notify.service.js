ec5d780593284966c9196632f983aae070fc4f17
Robbie Mackay <rm@robbiemackay.com>
2016-09-14 15:14:04 +1200

    Process templates and extra image paths

--- app/common/notifications/notify.service.js
+++ app/common/notifications/notify.service.js
@@ -148,7 +148,7 @@ function Notify(_, $q, $rootScope, $translate, SliderService, ModalService) {
                     '<div class="form-field">' +
                     '    <button class="button-flat" ng-click="$parent.cancel()" translate="message.button.cancel">Cancel</button>' +
                     '    <button class="button-destructive button-flat" ng-click="$parent.confirm()">' +
-                    '    <svg class="iconic"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#trash"></use></svg>' +
+                    '    <svg class="iconic"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/iconic-sprite.svg#trash"></use></svg>' +
                     '    <span translate="app.delete">Delete</span>' +
                     '    </button>' +
                     '</div>',
@@ -167,7 +167,7 @@ function Notify(_, $q, $rootScope, $translate, SliderService, ModalService) {
                 '<div class="form-field">' +
                 '    <button class="button-beta button-flat" ng-click="$parent.cancel()">Cancel</button>' +
                 '    <button class="button-destructive button-flat" ng-click="$parent.confirm()">' +
-                '    <svg class="iconic"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="../../img/iconic-sprite.svg#trash"></use></svg>' +
+                '    <svg class="iconic"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/iconic-sprite.svg#trash"></use></svg>' +
                 '    <span translate="app.delete">Delete</span>' +
                 '    </button>' +
                 '</div>', confirmText, false, scope, false, false);
